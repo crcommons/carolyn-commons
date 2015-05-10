12a2481673cade7284088f17c4261251281ec8b4
crcommons <crcommons@gmail.com>
2015-05-10 16:59:28 -0400

    slick player update
    making the slick play at autospead.

--- js/main.js
+++ js/main.js
@@ -15,8 +15,8 @@ suggestions are welcome!
 
 $(document).ready(function(){
   $('.jobs').slick({
-      // autoplay: true,
-      //  autoplaySpeed: 2000,
+      autoplay: true,
+       autoplaySpeed: 2000,
   });
 });
 
